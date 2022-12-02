ec79e447643861ca972da94ca673382e5256b738
Tales Cione <talesj@gmail.com>
2022-12-02 23:30:52 +0100

    Done solution 01.02
    Had to get the 3 biggest callories from all elves

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,30 @@ use std::fs;
 
 fn main() {
     let content = fs::read_to_string("./src/01/input.txt").expect("Not able to read input");
-    let group_max : i32 = content.split("\n\n").map(|group| {
-        let foods = group.split("\n");
-        foods.fold(0, |accum, item| {
-            let item_int : i32 = match item.parse() {
-                Ok(i) => i,
-                _ => 0
-            };
-            accum + item_int
+    let mut groups : Vec<i32> =
+        content
+        .split("\n\n")
+        .map(|group| {
+            let foods = group.split("\n");
+            foods.fold(0, |accum, item| {
+                let item_int : i32 = match item.parse() {
+                    Ok(i) => i,
+                    _ => 0
+                };
+                accum + item_int
+            })
         })
-    }).max().unwrap();
-    println!("{group_max}");
+        .collect::<Vec<i32>>();
+
+    groups.sort();
+
+    let len = groups.len();
+    let mut i = len - 1;
+    let mut sum = 0;
+    while i > len - 4 {
+        sum += groups[i];
+        i = i - 1;
+    }
+
+    println!("{sum}");
 }
