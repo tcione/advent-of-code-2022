bcab008d15b77a5e5c489d1cb6f09337c200d50d
Tales Cione <talesj@gmail.com>
2022-12-03 11:32:14 +0100

    day02: bootstrap

---
+++ src/day_02/mod.rs
@@ -0,0 +1,17 @@
+use std::fs;
+use std::path::Path;
+
+fn get_input() {
+    let input = Path::new("./day_02/input.txt");
+    fs::read_to_string(input).expect("Not able to read input");
+}
+
+#[allow(dead_code)]
+pub fn solution1() {
+    panic!("Not implemented");
+}
+
+#[allow(dead_code)]
+pub fn solution2() {
+    panic!("Not implemented");
+}
