3073286c896ab825a43d11f0cd7cf66929c08cfe
Tales Cione <talesj@gmail.com>
2022-12-03 12:25:35 +0100

    day02: solution for first exercise
    I first tried to go through a super structured approach (like defining
    proper types for scenarios and all). That said, I noticed I was overengineering
    the thing and retreated to good ole break, match, fold (reduce).

--- src/day_02/mod.rs
+++ src/day_02/mod.rs
@@ -1,14 +1,43 @@
 use std::fs;
 use std::path::Path;
 
-fn get_input() {
+// A X - Rock
+// B Y - Paper
+// C Z - Scissors
+//
+// X awards 1pt
+// Y awards 2pt
+// Z awards 3pt
+//
+// 0pt for losing
+// 3pt for draw
+// 6pt for winning
+
+fn get_input() -> String {
     let input = Path::new("./day_02/input.txt");
-    fs::read_to_string(input).expect("Not able to read input");
+    fs::read_to_string(input).unwrap()
+}
+
+fn points_for_scenario(raw_scenario: &str) -> u32 {
+    match raw_scenario {
+        "A X" => 1 + 3,
+        "A Y" => 2 + 6,
+        "A Z" => 3 + 0,
+        "B X" => 1 + 0,
+        "B Y" => 2 + 3,
+        "B Z" => 3 + 6,
+        "C X" => 1 + 6,
+        "C Y" => 2 + 0,
+        "C Z" => 3 + 3,
+        _ => 0,
+    }
 }
 
 #[allow(dead_code)]
-pub fn solution1() {
-    panic!("Not implemented");
+pub fn solution1() -> u32 {
+    get_input().split("\n").fold(0, |accum, item| {
+        accum + points_for_scenario(item)
+    })
 }
 
 #[allow(dead_code)]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod day_01;
+mod day_02;
 
 fn main() {
-    let solution = day_01::solution2();
-    println!("Day 01, Solution 02: {solution}");
+    // let solution = day_01::solution2();
+    // println!("Day 01, Solution 02: {solution}");
+
+    let solution = day_02::solution1();
+     println!("Day 02, Solution 01: {solution}");
 }
